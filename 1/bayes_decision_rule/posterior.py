f4494990066cfcc88219a25b31650830e30bf77b
Zhuo Chen <chenzhuo757@zju.edu.cn>
2020-05-11 19:24:14 +0800

    init

---
+++ 1/bayes_decision_rule/posterior.py
@@ -0,0 +1,21 @@
+import numpy as np
+from likelihood import likelihood
+
+def posterior(x):
+    '''
+    POSTERIOR Two Class Posterior Using Bayes Formula
+    INPUT:  x, features of different class, C-By-N vector
+            C is the number of classes, N is the number of different feature
+    OUTPUT: p,  posterior of each class given by each feature, C-By-N matrix
+    '''
+
+    C, N = x.shape
+    l = likelihood(x)
+    total = np.sum(x)
+    p = np.zeros((C, N))
+    #TODO
+
+    # begin answer
+    # end answer
+    
+    return p
---
+++ 1/gaussian_discriminant/gaussian_pos_prob.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def gaussian_pos_prob(X, Mu, Sigma, Phi):
+    '''
+    GAUSSIAN_POS_PROB Posterior probability of GDA.
+    Compute the posterior probability of given N data points X
+    using Gaussian Discriminant Analysis where the K gaussian distributions
+    are specified by Mu, Sigma and Phi.
+    Inputs:
+        'X'     - M-by-N numpy array, N data points of dimension M.
+        'Mu'    - M-by-K numpy array, mean of K Gaussian distributions.
+        'Sigma' - M-by-M-by-K  numpy array (yes, a 3D matrix), variance matrix of
+                  K Gaussian distributions.
+        'Phi'   - 1-by-K  numpy array, prior of K Gaussian distributions.
+    Outputs:
+        'p'     - N-by-K  numpy array, posterior probability of N data points
+                with in K Gaussian distribsubplots_adjustutions.
+    ''' 
+    N = X.shape[1]
+    K = Phi.shape[0]
+    p = np.zeros((N, K))
+    #Your code HERE
+
+    # begin answer
+    # end answer
+    
+    return p
+    
